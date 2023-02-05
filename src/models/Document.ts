5c1795a6aebc37e3128139e23185a9ccfcb1cca6
Thebigbot <77632836+athebigbot@users.noreply.github.com>
2023-02-05 09:31:28 +0100

    feat(api)!: get document endpoint added

--- src/models/Document.ts
+++ src/models/Document.ts
@@ -2,16 +2,16 @@ import type { Document, Model } from "mongoose";
 import { Schema, model, models } from "mongoose";
 
 interface DocumentInterface extends Document {
-    id: string;
-    content: string;
+  id: string;
+  content: string;
 }
-  
 
 const DocSchema = new Schema<DocumentInterface>({
-    id: String,
-    content: String
+  id: String,
+  content: String,
 });
 
-const Doc: Model<DocumentInterface> = models.Document || model<DocumentInterface>('Document', DocSchema);
+const Doc: Model<DocumentInterface> =
+  models.Document || model<DocumentInterface>("Document", DocSchema);
 
 export { Doc };
